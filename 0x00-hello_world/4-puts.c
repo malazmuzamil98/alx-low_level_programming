6e540c9e3191f388e9ab19ee4f314241d296779a
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-05 12:01:14 -0700

    $var

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main - A program that prints a line using puts
+ *
+ * Description: prints Programming is like building a multilingual puzzle.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
-		
-		return(0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
