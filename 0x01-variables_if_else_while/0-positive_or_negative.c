6ae93996cc226768e60604acc12daa4cf8db86b7
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-06 09:29:07 -0700

    $var

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("is positive\n");
+		puts("is positive");
 	if (n == 0)
-		printf("is zero\n");
+		puts("is zero");
 	if (n > 0)
-		printf("is negetive\n");
+		puts("is negetive");
 	return (0);
 }
--- 0x01-variables_if_else_while/test.c
+++ 0x01-variables_if_else_while/test.c
@@ -3,6 +3,6 @@ int main(void)
 {
 	int n;
 	if ( n > 0 )
-		printf("%S, "is positive"\n");
+		printf("is positive\n");
 	return 0;
 }
