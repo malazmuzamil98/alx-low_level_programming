47f5aad85a5d3d3f2d5209ac017273b9242bb817
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-06 16:08:53 -0700

    $var

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print double digit combos
  *
@@ -6,12 +7,12 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i, j;
 
 	i = 48;
-
 	j = 48;
 
 	while (i < 58)
@@ -20,7 +21,6 @@ int main(void)
 		while (j < 58)
 		{
 			putchar(i);
-
 			putchar(j);
 
 			if (i < 56 || j < 57)
@@ -28,11 +28,13 @@ int main(void)
 				putchar(44);
 				putchar(32);
 			}
-
+			j++;
 		}
-		j++;
+
+		i++;
 	}
-	i++;
+
 	putchar(10);
+
 	return (0);
 }
--- 0x01-variables_if_else_while/test.c
+++ 0x01-variables_if_else_while/test.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 
 /**
- * main - print single digit numbers
+ * main - print double digit combos
  *
- * Description: print single digit numbers
+ * Description: print double digit combos
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i;
+	int i, j;
 
 	i = 48;
+	j = 48;
 
 	while (i < 58)
 	{
-		putchar(i);
+		j = i + 1;
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i < 56 || j < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			j++;
+		}
+
 		i++;
 	}
 
