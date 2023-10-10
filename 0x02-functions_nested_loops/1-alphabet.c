74aa7eab54792243ef29c7eff7fd124dc88ce627
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-10 11:30:47 -0700

    $var

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A program that prints alphabet in lower case.
+ *
+ * Description: using the header file main.h
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+		for (alpha = 97; alpha <= 122; alpha++)
+		{
+			_putchar(alpha);
+			_putchar(10);
+		}
+}
+
