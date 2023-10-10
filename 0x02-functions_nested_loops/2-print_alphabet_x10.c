62a45bb8cf5c437ee169fcb05756d76e2d7c5666
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-10 13:42:56 -0700

    $var

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,10 @@
 #include "main.h"
-/**
-* print_alphabet_x10 - prints the alphabet 10 times
-*
-* print_alphabet - prints alphabet in lower case
-*
-* Return: Always 0
-*/
 
-void print_alphabet(void);
+/**
+* print_alphabet_x10 - print the alphabest 10 times
+*
+* Return: void here
+*/
 
 void print_alphabet_x10(void)
 
@@ -16,16 +13,12 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-	print_alphabet();
-	}
-}
-void print_alphabet(void)
-{
-	int alpha;
+		int alpha;
 
-	for (alpha = 97; alpha <= 122; alpha++)
-	{
-		_putchar(alpha);
+		for (alpha = 97; alpha <= 122; alpha++)
+		{
+			_putchar(alpha);
+		}
 	}
 	_putchar(10);
 }
