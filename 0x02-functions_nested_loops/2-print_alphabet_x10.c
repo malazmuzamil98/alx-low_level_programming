0026672bc5975e52609f283411e653c36bbf5937
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-10 12:29:20 -0700

    $var

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  print_alphabet_x10 - prints the alphabet 10 times
+ *
+ *  Return: Always 0.
+ */
+void print_alphabet(void);
+
+void print_alphabet_x10(void)
+
+{
+	int i = 0;
+
+	for (i = 0; i <= 10; i++)
+	{
+	print_alphabet();
+	}
+}
