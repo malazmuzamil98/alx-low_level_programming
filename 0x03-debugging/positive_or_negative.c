58a254f602333805d3ca5f8db8b9a6303413bdbe
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-12 01:52:30 +0300

    $var

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success
+ */
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+void positive_or_negetive(int i)
+{
+	if (i >= 0)
+		printf("%d is positive\n", i);
+	else
+		printf("%d is negative\n", i);
+}
