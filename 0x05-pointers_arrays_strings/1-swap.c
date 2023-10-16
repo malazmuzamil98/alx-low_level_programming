8e5a4f82dd78405497441ff13b59933bcbfddb41
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-16 05:55:41 -0700

    $var

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * swap_int - swaps the values of a and b
+  *
+  * @a: pointer a
+  *
+  * @b: pointer b
+  *
+  * Return: Always 0.
+  */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
