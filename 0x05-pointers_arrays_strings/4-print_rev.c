939ee5ad377e77f5cc3f6221f9a13d98e0204259
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-16 08:30:22 -0700

    $var

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: string parameter
+ *
+ * Return: Always 0.
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
