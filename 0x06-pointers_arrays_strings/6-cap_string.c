fa58877f3b699d2c035af42077218d7660b06535
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-19 11:39:38 -0700

    $var

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The input of the function.
+ *
+ * Return: A pointer to the changed string.
+ */
+
+char *cap_string(char *str)
+{
+	int in = 0;
+
+
+	while (str[in])
+	{
+		while (!(str[in] >= 'a' && str[in] <= 'z'))
+			in++;
+
+		if (str[in - 1] == ' ' ||
+		    str[in - 1] == '\t' ||
+		    str[in - 1] == '\n' ||
+		    str[in - 1] == ',' ||
+		    str[in - 1] == ';' ||
+		    str[in - 1] == '.' ||
+		    str[in - 1] == '!' ||
+		    str[in - 1] == '?' ||
+		    str[in - 1] == '"' ||
+		    str[in - 1] == '(' ||
+		    str[in - 1] == ')' ||
+		    str[in - 1] == '{' ||
+		    str[in - 1] == '}' ||
+		    in == 0)
+			str[in] -= 32;
+
+		in++;
+	}
+
+	return (str);
+}
