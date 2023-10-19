557f652f7cbf27ae3f3f5f7fe73d6af39d18bd1a
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-19 07:29:13 -0700

    $var

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * reverse_array - Entry point
+  * @a: integers array
+  *@n: elements in the array
+  */
+void reverse_array(int *a, int n)
+{
+	int len;
+
+	int i = 0;
+
+	int temp;
+
+	if (n % 2 != 0)
+		len = ((n - 1) / 2);
+	else
+		len = n / 2;
+	while (a[i] < len)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
