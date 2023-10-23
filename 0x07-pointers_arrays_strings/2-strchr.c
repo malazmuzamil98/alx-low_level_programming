a6a9ca59107c194dd4988062da5e6443097f6555
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-23 15:52:04 -0700

    $var

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,14 +11,16 @@
   */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	while (s[i] != c)
 	{
-		if (*s == c)
+		if (s[i] == '\0')
 		{
-			return (s);
+			return (0);
 		}
-		s++;
+		i++;
 	}
-	return (0);
+	return (s + i);
 
 }
