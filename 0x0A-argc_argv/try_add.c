3fcbf342e15ac35b1bb0704034e6262162ffd373
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-27 23:24:13 +0200

    add

--- 0x0A-argc_argv/try_add.c
+++ 0x0A-argc_argv/try_add.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * isPure - a function checks if the argument is pure integer or not
+ * @s: string argument
+ * Return: 0 for success 1 for failer
+*/
+int isPure(char *s)
+{
+	int ck;
+
+	for (ck = 0; s[ck] != '\0'; ck++)
+	{
+		if (s[ck] <= '0' || s[ck] >= '9')
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+/**
  * main - Entry point
  * @argc: number of arguments
  * @argv: a pointer of array arguments
@@ -19,7 +37,7 @@ int main(int argc, char *argv[])
 		sum = 0;
 		while (i < argc)
 		{
-			if (*argv[i] > 48 && *argv[i] < 58)
+			if (isPure(argv[i]) == 0)
 		{
 			sum += atoi(argv[i]);
 		}
@@ -28,7 +46,7 @@ int main(int argc, char *argv[])
 			printf("Error\n");
 			return (1);
 		}
-		i++; 
+		i++;
 		}
 		printf("%d\n", sum);
 	}
