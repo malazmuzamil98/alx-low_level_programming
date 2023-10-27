0a445f77eb41f1b5dafc4df8e42e4c30bffc271c
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-27 12:37:43 +0200

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: a pointer of array arguments
+ * Return: nothing
+*/
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
