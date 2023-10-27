4d82dbce10e5cdeb119532375254ae712bb03338
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-27 21:41:07 +0200

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,5 +7,22 @@
 */
 int main(int argc, char *argv[])
 {
-	
+	int i, value, cents;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	i = cents = 0;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	value = atoi(argv[1]);
+	while (value > 0 && i < 5)
+	{
+		cents += value / coins[i];
+		value = value % coins[i];
+		i++;
+	}
+	printf("%d\n", cents);
+	return (0);
 }
