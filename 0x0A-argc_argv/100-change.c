477cd36cef3ffe944d35bfccfb4ef25693561ac2
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-27 18:37:27 +0200

    add

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: a pointer of array arguments
+ * Return: nothing
+*/
+int main(int argc, char *argv[])
+{
+	
+}
--- 0x0A-argc_argv/try_add.c
+++ 0x0A-argc_argv/try_add.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 			printf("Error\n");
 			return (1);
 		}
-		i++;
+		i++; 
 		}
 		printf("%d\n", sum);
 	}
