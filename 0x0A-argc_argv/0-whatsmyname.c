0b59bc18ffca9f23f646e6b8d8bb84e35aff1bb5
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-27 12:10:35 +0200

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: a pointer of array arguments
+ * Return: nothing
+*/
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
