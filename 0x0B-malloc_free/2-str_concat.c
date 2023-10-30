adb00b3f6a16654a8cf7e8d6ec8f56c8a887d9a3
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-30 22:09:30 +0200

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,31 @@
 #include "main.h"
 /**
- * str_concat - a function that concatenates two strings
- * @s1: first string
- * @s2: second string
- * Return: pointer to the string
-*/
+ * str_concat - concatenates two strings.
+ * @s1: string1
+ * @s2: string2
+ * Return: 0 or s3
+ */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int len1 = 0;
-	unsigned int len2 = 0;
 	char *cat;
+	unsigned int len1 = 0, len2 = 0;
 
 	if (s1 == 0)
-	{
 		s1 = "";
-	}
 	if (s2 == 0)
-	{
 		s2 = "";
-	}
-	while (s1 && s1[len1])
-	{
+	while (s1[len1] != '\0')
 		len1++;
-	}
-	while (s2 && s2[len2])
-	{
+	while (s2[len2] != '\0')
 		len2++;
-	}
-	cat = malloc((len1 + len2 + 1) * sizeof(char));
-
+	len2 += len1;
+	cat = malloc(sizeof(char) * (len2 + 1));
 	if (cat == 0)
-	{
 		return (0);
-	}
 	for (len1 = 0; s1[len1] != '\0'; len1++)
-	{
 		cat[len1] = s1[len1];
-	}
-	for (len2 = 0; *s2 != '\0' ; len2++)
-	{
-		cat[len1] = *s2;
-		s2++;
-	}
+	for (len2 = 0; s2[len2] != '\0'; len1++, len2++)
+		cat[len1] = s2[len2];
 	cat[len1] = '\0';
 	return (cat);
 }
