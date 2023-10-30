f83cc2fda192cb6b6a677a90dc70b546a42d6a12
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-30 17:53:58 +0200

    strdup

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * free_grid - a function  frees a 2 dimensional grid
+ * @grid: 2D array 
+ * @height: array height
+ * Return: Nothing
+*/
+void free_grid(int **grid, int height)
+{
+	
+}
