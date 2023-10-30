96d82548054889f41a712d7261077923572b6bb2
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-30 12:24:42 +0200

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,13 +7,9 @@
 */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-	{
-		return (NULL);
-	}
 	unsigned int i;
 	char *ar = (char *)malloc(size * sizeof(char));
-
+	
 	if (ar == NULL)
 	{
 		return (NULL);
