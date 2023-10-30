76ed239bb0d55015c62f39db470a0841d1697d94
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-10-30 19:04:47 +0200

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,11 +16,11 @@ char *_strdup(char *str)
 	}
 	new = malloc((len + 1) * sizeof(char));
 
-	if (new == NULL)
+	if (new == 0)
 	{
-		return (NULL);
+		return (0);
 	}
-	for (i = 0; i < len; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		new[i] = str[i];
 	}
