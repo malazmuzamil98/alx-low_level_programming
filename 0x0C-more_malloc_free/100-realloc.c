2c733e54ad600fb2da424fa16d50190a57b0048a
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-02 20:02:07 +0200

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,5 +8,49 @@
 */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	
+	void *ptr1;
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+		{
+			printf("failed to allocate memory\n");
+			return (NULL);
+		}
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		ptr1 = malloc(new_size);
+		if (ptr1 == NULL)
+		{
+			printf("failed to allocate memory\n");
+			return (NULL);
+		}
+		memcpy(ptr1, ptr, old_size);
+		free(ptr);
+		return (ptr1);
+	}
+	if (new_size < old_size)
+	{
+		ptr1 = malloc(new_size);
+		if (ptr1 == NULL)
+		{
+			printf("failed to allocate memory\n");
+			return (NULL);
+		}
+		memcpy(ptr1, ptr, new_size);
+		free(ptr);
+		return (ptr1);
+	}
+	return (NULL);
 }
