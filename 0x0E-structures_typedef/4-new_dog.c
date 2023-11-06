09d90de4751c3d63b6946e3ca299f66e581802fe
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-06 11:56:25 +0200

    task0

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * new_dog -  a function that creates a new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,16 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-struct dog {
+/**
+ * struct dog - a structure holds information about dogs
+ * @name: first member
+ * @age: second member
+ * @owner: theird member
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
-int _putchar(char c);
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-/*void print_dog(struct dog *d);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);*/
+void free_dog(dog_t *d);
 
 #endif
