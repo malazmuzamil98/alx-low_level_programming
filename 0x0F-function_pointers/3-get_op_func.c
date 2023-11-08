fe5e190cd4966ed180d74535fecbe109fa9751a4
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-08 23:40:47 +0200

    task3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+/**
+ * get_op_function - a function pointer
+ * @s: operator passed
+ * Return: operator result
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+    while (i < 5)
+	{
+		if (ops[i].op[0] == *s && strlen(s) == 1)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (0);
+}
