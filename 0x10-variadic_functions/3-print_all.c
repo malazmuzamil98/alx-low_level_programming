c48f83b9cab6798b0c772ee257c2d3ff4898574b
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-09 21:57:30 +0200

    d

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -60,17 +60,22 @@ void print_all(const char * const format, ...)
 	int i, j;
 	va_list all;
 
-	arg_num = strlen(format);
-	i = 0;
-	va_start(all, format);
-	while (i < arg_num)
+	if (format == NULL)
 	{
-		j = 1;
-		handle_format(format[i], &all, &j);
-		if (j != 0 && i < arg_num - 1)
-			printf(", ");
-		i++;
+		printf("\n");
+		return;
 	}
-	printf("\n");
-	va_end(all);
+		arg_num = strlen(format);
+		i = 0;
+		va_start(all, format);
+		while (i < arg_num)
+		{
+			j = 1;
+			handle_format(format[i], &all, &j);
+			if (j != 0 && i < arg_num - 1)
+				printf(", ");
+			i++;
+		}
+		printf("\n");
+		va_end(all);
 }
