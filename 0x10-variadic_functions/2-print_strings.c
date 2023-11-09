8cd6e76f9b6dc990d887d23fb754d177be33fb80
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-09 15:54:35 +0200

    c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			x = "nil";
 		}
 		printf("%s", x);
-		if (i <= n - 1)
+		if (i < n - 1)
 		{
 			printf("%s", separator);
 		}
