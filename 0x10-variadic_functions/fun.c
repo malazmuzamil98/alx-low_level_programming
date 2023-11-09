2bb71216418eec7756a86baaa6a0884c8e4b4f7f
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-09 21:17:33 +0200

    d

--- 0x10-variadic_functions/fun.c
+++ 0x10-variadic_functions/fun.c
@@ -49,28 +49,3 @@ void handle_format(char format, va_list *all, int *flag)
 		}
 	}
 }
-/**
- * print_all - a function that prints anything
- * @format: a list of types of arguments passed to the function
- * Return: Nothing
-*/
-void print_all(const char * const format, ...)
-{
-	int arg_num;
-	int i, j;
-	va_list all;
-
-	arg_num = strlen(format);
-	i = 0;
-	va_start(all, format);
-	while (i < arg_num)
-	{
-		j = 1;
-		handle_format(format[i], &all, &j);
-		if (j != 0 && i < arg_num - 1)
-			printf(", ");
-		i++;
-	}
-	printf("\n");
-	va_end(all);
-}
