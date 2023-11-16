d35cc77c9f4fbedcc9cfc9c5169ca746c49f050b
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-16 21:40:02 +0200

    modified task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,10 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 			n++;
 		}
-		printf("[%u] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
 	}
 	n++;
