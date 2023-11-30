bc3490d5492ba34be98fa630209891829ab5998f
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-30 23:39:57 +0200

    task0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,1 @@
+#include "test/task0.c"
---
+++ 0x13-more_singly_linked_lists/test/task0.c
@@ -0,0 +1,27 @@
+#include "../lists.h"
+/**
+ * print_list - prints all elements
+ * @h: head
+ * Return: count
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t n;
+	n =0;
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			n++;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			n++;
+		}
+		h = h->next;
+	}
+
+	return (n);
+}
