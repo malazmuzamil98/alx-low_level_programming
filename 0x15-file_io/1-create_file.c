fe0646e299aded0a8ea669abbf63b427c21b8f38
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-30 03:43:32 +0200

    task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file
+ * @filename: name of the file to create
+ * @text_content: is a NULL terminated string
+ * Return: 1
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int x;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	x = write(fd, text_content, strlen(text_content));
+	if (x == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
