09c56ca6b289563b94dc02e22d056223b6fb8b0c
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-11-30 23:53:36 +0200

    all

--- 0x13-more_singly_linked_lists/test/task0.c
+++ 0x13-more_singly_linked_lists/test/task0.c
@@ -1,15 +1,8 @@
 #include "../lists.h"
 
-/**
- * print_listint - print all list
- * @h: pointer to head
- * Return: size of list
-*/
-
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
-
 	while (h)
 	{
 		printf("%d\n", h->n);
