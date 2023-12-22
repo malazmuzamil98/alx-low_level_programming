c06a8ee942f83a73e79634fc5c37bba0251d6a0c
malazmuzamil98 <malazmuzamil98@gmail.com>
2023-12-22 16:58:20 +0200

    fixed task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,10 +19,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if text_content != NULL
+	if (text_content != NULL)
 		x = write(fd, text_content, strlen(text_content));
 	if (x == -1)
 	{
+		close(fd);
 		return (-1);
 	}
 	close(fd);
