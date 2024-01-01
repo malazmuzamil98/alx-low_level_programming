44e1ac44960086d4c1702c309e8947e2743e83fa
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-01 23:44:06 +0200

    all

--- betty/install.sh
+++ betty/install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
+
+# clear the terminal
 clear
-sudo cp *.py /bin/
+# check if python3 is installed
+if ! command -v python3 &> /dev/null; then
+	# install python3 if not installed
+	sudo apt-get update
+	sudo apt-get install -y python3
+fi
+ # copy all python scripts to ~ with a dot prefix to make them hidden
+for script in *.py dot
+	sudo cp "$script" "~/.${script}"
+done
+
+# Add an alias to .bashrc
 echo "alias fix='python3 /bin/betty_fixer.py'" >> ~/.bashrc
-echo "Ready to use the spectacular Betty Fixer ..."
-echo "Created by MoeAlsir"
+
+# Inform the user about the changes
+echo -e "please copy and run this command\n"
+echo -e "          source ~/.bashrc"
+read -p "Press [Enter] key to continue ..."
+
+clear
+
+echo -e "Usage:\n"
+echo -e "      fix file1.c file2.c .... \n"
