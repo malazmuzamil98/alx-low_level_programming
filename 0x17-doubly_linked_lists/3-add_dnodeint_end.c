4a29de493547b7c62faf61876d612162e78a826a
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-20 02:45:44 +0200

    task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,6 +23,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	temp = *head;
 	if (temp != NULL)
 	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
 		temp->next = new_node;
 	}
 	else
