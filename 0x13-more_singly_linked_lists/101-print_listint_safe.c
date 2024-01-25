321ea0c784694d8efc085ef9b4ff6286aef3c2f4
malazmuzamil98 <malazmuzamil98@gmail.com>
2024-01-25 09:59:16 +0200

    advance

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,78 +1,39 @@
 #include "lists.h"
-/**
- * ncalc - calculate real nodes
- *
- * @head: ptr
- *
- * Return: nodes
- */
-size_t ncalc(const listint_t *head)
-{
-	const listint_t *atptr, *postptr;
-	size_t count = 1;
 
-	if (!head || !head->next)
-		return (0);
-	atptr = head->next;
-	postptr = atptr->next;
-	while (postptr)
-	{
-		if (atptr == postptr)
-		{
-			atptr = head;
-			while (atptr != postptr)
-			{
-				count++;
-				atptr = atptr->next;
-				postptr = postptr->next;
-			}
-			atptr = atptr->next;
-			while (atptr != postptr)
-			{
-				count++;
-				atptr = atptr->next;
-			}
-			return (count);
-		}
-		atptr = atptr->next;
-		postptr = (postptr->next)->next;
-	}
-	return (0);
-}
 /**
- * print_listint_safe - prints a listint_t linked list.
- *
- * @head: ptr
- *
- * Return: the number of nodes in the list
+ * print_listint_safe - prints linked list safe
+ * @head: head pointer
+ * Return: the loop
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *ptr;
-	size_t n, i = 0;
+	const listint_t *corrent = NULL;
+	const listint_t *check = NULL;
+	size_t counter = 0;
+	size_t counter_check;
 
-	if (!head)
-		return (0);
-	ptr = head;
-	n = ncalc(head);
-	if (n == 0)
+	/*check head*/
+
+	corrent = head;
+	while (corrent)
 	{
-		while (ptr)
+		if (!head)
+			exit(98);
+		printf("[%p] %d\n", (void *)corrent, corrent->n);
+		counter++;
+		corrent = corrent->next;
+		check = head;
+		counter_check = 0;
+		while (counter_check < counter)
 		{
-			printf("[%p] %d\n", (void *)ptr, ptr->n);
-			n++;
-			ptr = ptr->next;
+			if (check == corrent)
+			{
+				printf("-> [%p] %d\n", (void *)corrent, corrent->n);
+				return (counter);
+			}
+			check = check->next;
+			counter_check++;
 		}
-		return (n);
 	}
-	else
-	{
-		for (; i < n; i++)
-		{
-			printf("[%p] %d\n", (void *)ptr, ptr->n);
-			ptr = ptr->next;
-		}
-		printf("->[%p] %d\n", (void *)ptr, ptr->n);
-		return (n);
-	}
+	return (counter);
 }
